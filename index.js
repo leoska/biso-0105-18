ab9c4764de42d9afcb418986732a5a0324060bdb
Leonid <Leo77551@gmail.com>
2022-03-12 11:54:32 +0300

    first commit http nodejs express serrverr

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const port = 25665;
+
+const app = express();
+const chat = [];
+
+app.use(express.json());
+
+app.get('/getChat', (req, res) => {
+    res.json(chat);
+});
+
+app.post('/sendMessage', (req, res) => {
+    const data = req.body;
+
+    data.stamp = Date.now();
+
+    chat.push(data);
+    res.json({response: true});
+});
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
